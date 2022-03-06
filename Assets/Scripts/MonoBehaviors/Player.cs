56e0163284edaf2e1882c17b78bd7ef0a9f227b7
jazheng <jazheng@davidson.edu>
2022-03-06 17:27:31 -0500

    Chapter up to 6 Finished

--- Assets/Scripts/MonoBehaviors/Player.cs
+++ Assets/Scripts/MonoBehaviors/Player.cs
@@ -4,15 +4,60 @@ using UnityEngine;
 
 public class Player : Character
 {
-    // Start is called before the first frame update
+    public HealthBar healthBarPrefab;
+    HealthBar healthBar;
+    public Inventory inventoryPrefab;
+    Inventory inventory;
+
     void Start()
     {
-        
+        inventory = Instantiate(inventoryPrefab);
+
+        hitPoints.value = startingHitPoints;
+        healthBar = Instantiate(healthBarPrefab);
+        healthBar.character = this;
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.gameObject.CompareTag("CanBePickedUp"))
+        {
+            Item hitObject = collision.gameObject.GetComponent<Consumable>().item;
+
+            if (hitObject != null)
+            {
+                bool shouldDissappear = false;
+
+                switch (hitObject.itemType)
+                {
+                    case Item.ItemType.COIN: 
+                        shouldDissappear = inventory.AddItem(hitObject);
+                        break;
+                    case Item.ItemType.HEALTH:
+                        shouldDissappear = AdjustHitPoints(hitObject.quantity);
+                        break;
+                    default:
+                        break;
+                }
+                if(shouldDissappear)
+                {
+                    collision.gameObject.SetActive(false);
+                }
+            }
+
+        }
+    }
+
+    public bool AdjustHitPoints(int amount)
+    {
+        if(hitPoints.value < maxHitPoints)
+        {
+            hitPoints.value = hitPoints.value + amount;
+            print("Adjusted hitpoints by: " + amount + ". New value: " + hitPoints.value);
+            
+            return true;
+        }
+        return false;
     }
 }
+
